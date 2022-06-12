712821f1b6a7b11c2b000e31de4aaa34dea3cf33
leetaewoo <taewoo5131@gmail.com>
2022-06-12 16:15:08 +0900

    [트랜잭션 이해] - 트랜잭션 템플릿 사용

---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -0,0 +1,48 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 템플릿
+ */
+@Service
+public class MemberServiceV3_2 {
+//    private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate txTemplate;
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        this.txTemplate.executeWithoutResult((status) -> {
+            try {
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+        memberRepository.update(fromMember.getMemberId() , fromMember.getMoney() - money);
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생 !!!!");
+        }
+        memberRepository.update(toMember.getMemberId(), toMember.getMoney() + money);
+    }
+}
