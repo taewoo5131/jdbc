91bc58601f623c4dd8fc162e314414ef5ae1f2d3
leetaewoo <taewoo5131@gmail.com>
2022-06-17 13:20:55 +0900

    [예외 이해] - Repository 인터페이스 적용 ( SQLException에 종속 X )

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV4_1.java
@@ -0,0 +1,119 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.ex.MyDBException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+ * 예외 누수 문제 해결
+ * 체크 예외를 런타임 예외로 변경
+ * MemberRepository 인터페이스 사용
+ * throws Exception 제거
+ */
+@Slf4j
+public class MemberRepositoryV4_1 implements MemberRepository{
+    private final DataSource dataSource;
+    public MemberRepositoryV4_1(DataSource dataSource) {
+
+        this.dataSource = dataSource;
+    }
+
+    //save()...
+    @Override
+    public Member save(Member member){
+        String sql = "insert into member values(?,?)";
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        try {
+            connection = getConnection();
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            throw new MyDBException(e);
+        } finally {
+            close(connection,pstmt,null);
+        }
+    }
+
+    //findById()...
+    public Member findById(String memberId){
+        String sql = "select * from member where member_id = ?";
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            connection = getConnection();
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member(rs.getString("member_id"), rs.getInt("money"));
+                return member;
+            } else{
+                throw new NoSuchElementException("member 없음");
+            }
+        } catch (SQLException e) {
+            throw new MyDBException(e);
+        } finally {
+            close(connection, pstmt, rs);
+        }
+    }
+
+    //update()....
+    public void update(String memberId, int money){
+        String sql = "update member set money = ? where member_id = ?";
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        try {
+            connection = getConnection();
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new MyDBException(e);
+        } finally {
+            close(connection,pstmt,null);
+        }
+    }
+
+    //delete()....
+    public void delete(String memberId){
+        String sql = "delete member where member_id = ?";
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        try {
+            connection = getConnection();
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new MyDBException(e);
+        } finally {
+            close(connection,pstmt,null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+
+        // 주의 ! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
+        DataSourceUtils.releaseConnection(con , dataSource);
+    }
+    private Connection getConnection() throws SQLException {
+        // 주의 ! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
+        Connection con = DataSourceUtils.getConnection(dataSource);
+        log.info("get connection={}, class={}", con, con.getClass());
+        return con;
+    }
+}
